61a13f1f9476a877838e0bbf9200672ac7341cef
sivavenkat554 <117220138+sivavenkat554@users.noreply.github.com>
2024-08-13 16:45:08 +0530

    Add files via upload

---
+++ venkat.js
@@ -0,0 +1,27 @@
+function yesClicked() {
+    document.getElementById('message').textContent = 'I know, just tell me a date Now ';
+    document.getElementById('noButton').style.display = 'none';
+}
+
+function noClicked() {
+    const noButton = document.getElementById('noButton');
+    const container = document.querySelector('.container');
+    
+  
+    const containerRect = container.getBoundingClientRect();
+    
+   
+    const buttonRect = noButton.getBoundingClientRect();
+    
+
+    const maxX = containerRect.width - buttonRect.width - 20; 
+    const maxY = containerRect.height - buttonRect.height - 20; 
+
+    
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    
+    noButton.style.transform = `translate(${randomX}px, ${randomY}px)`;
+}
+
